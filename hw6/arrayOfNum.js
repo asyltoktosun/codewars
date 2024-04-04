db1c27ce70b7841f17fc20dd37df12e677a87758
Asyl <toktosunovaasyl@gmail.com>
2024-04-04 11:14:10 +0300

    home work 6

---
+++ hw6/arrayOfNum.js
@@ -0,0 +1,17 @@
+// Write a function that takes an array of numbers and returns the sum of the numbers. 
+// The numbers can be negative or non-integer.
+//  If the array does not contain any numbers then you should return 0.
+
+
+
+const numberFunc=(sum, ...nums)=>{
+    if(nums.length===0){
+        return 0
+    }
+    for(let i=0; i<nums.length; i++){
+        sum+=nums[i]  
+    }
+    return sum
+}
+
+console.log(numberFunc(90,809));
